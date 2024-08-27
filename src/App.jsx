23c762e66d62ff5b7b63735c88ce89dc41a2d054
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-08-27 15:10:48 -0400

    fix
    removed a import image to fix the build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-        <a href="https://react.dev" target="_blank">
-        </a>
+        <a href="https://vitejs.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -24,7 +23,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
