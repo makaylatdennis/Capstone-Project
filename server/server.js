0f881049af8a4344a7e392eba622cd0af92a060f
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-09-02 20:25:49 -0400

    update
    updated endpoints

--- server/server.js
+++ server/server.js
@@ -1,41 +1,19 @@
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-const port = process.env.PORT || 5000;
-const chatbot = require("./server.chatbot");
-
-app.use(cors()); // Allow requests from different origins
-
-app.use(express.static(path.join(__dirname, "dist")));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.post("/chatbot", chatbot.chatbot);
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-const express = require("express");
-const path = require("path");
 require("dotenv").config();
 
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 6000;
-const chatbot = require("./server.chatbot");
+// const chatbot = require("./server.chatbot");
 const DB = require("./server.DB"); // this is for database connection and verification
+const cors = require("cors");
 
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api", router);
+app.use(cors()); // Allow requests from different origins
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
@@ -55,7 +33,15 @@ router.post("/logout", DB.auth.verify, DB.auth.logout);
 
 // event endpoints
 router.get("/events/:id", DB.auth.verifyAdmin, DB.events.getByID); // Protected API
+
+router.get("/events/approved", DB.auth.verifyAdmin, DB.events.getApproved);
+router.get("/events/pending", DB.auth.verifyAdmin, DB.events.getPending);
+router.get("/events/rejected", DB.auth.verifyAdmin, DB.events.getRejected);
+
 router.put("/events/:id", DB.auth.verifyAdmin, DB.events.update);
+router.put("/events/approve/:id", DB.auth.verifyAdmin, DB.events.approve);
+router.put("/events/reject/:id", DB.auth.verifyAdmin, DB.events.reject);
+
 router.post("/events", DB.auth.verifyAdmin, DB.events.create);
 router.delete("/events/:id", DB.auth.verifyAdmin, DB.events.delete);
 
@@ -64,15 +50,12 @@ router.get("/events", DB.auth.verify, DB.events.get);
 router.get("/events/user", DB.auth.verify, DB.events.getByUser);
 
 // volunteer event endpoints
-router.get("/events/volunteer/:id", DB.auth.verifyAdmin, DB.events.vol.getByID); // Protected API
-router.put("/events/volunteer/:id", DB.auth.verifyAdmin, DB.events.vol.update);
-router.delete(
-  "/events/volunteer/:id",
-  DB.auth.verifyAdmin,
-  DB.events.vol.delete
-);
+router.get("/volunteer", DB.auth.verifyAdmin, DB.vol.get); // Protected API
+router.get("/volunteer/:id", DB.auth.verifyAdmin, DB.vol.getByID);
+router.put("/volunteer/:id", DB.auth.verifyAdmin, DB.vol.update);
+router.delete("/volunteer/:id", DB.auth.verifyAdmin, DB.vol.delete);
 
-router.post("/events/volunteer", DB.auth.verifyAdmin, DB.events.vol.add); // Open API // Public
+router.post("/volunteer", DB.vol.add); // Open API // Public
 
 // contact endpoints
 router.get("/contacts", DB.auth.verifyAdmin, DB.contact.get); // Protected API
@@ -82,7 +65,7 @@ router.delete("/contacts/:id", DB.auth.verifyAdmin, DB.contact.delete);
 
 router.post("/contacts", DB.auth.verify, DB.contact.create); // Open API // Server Only
 
-//router.post("/chatbot", chatbot.chatbot);
+// router.post("/chatbot", chatbot.chatbot);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
