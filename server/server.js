c501a9c6e907df18c0e0307d136d49e52c8528e6
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-09-04 13:24:18 -0400

    update
    add admin example plage and endpoint upgrades

--- server/server.js
+++ server/server.js
@@ -21,15 +21,18 @@ app.get("*", (req, res) => {
 });
 
 // API Endpoints
+
+// chatbot endpoint
 app.post("/chatbot", chatbot.chatbot);
+
 // user endpoints
 router.get("/users", DB.auth.verifyAdmin, DB.users.get); // Protected API
 router.get("/users/:id", DB.auth.verifyAdmin, DB.users.getByID);
 router.put("/users/:id", DB.auth.verifyAdmin, DB.users.update);
 router.delete("/users/:id", DB.auth.verifyAdmin, DB.users.delete);
 
-router.post("/signup", DB.auth.signup); // Open API // Server Only
-router.post("/login", DB.auth.login);
+router.post("/signup", DB.auth.signup); // Open API // Server Only // Zero Auth
+router.post("/login", DB.auth.login); // Open API // Server Only // Zero Auth
 router.post("/logout", DB.auth.verify, DB.auth.logout);
 
 // event endpoints
@@ -46,9 +49,23 @@ router.put("/events/reject/:id", DB.auth.verifyAdmin, DB.events.reject);
 router.post("/events", DB.auth.verifyAdmin, DB.events.create);
 router.delete("/events/:id", DB.auth.verifyAdmin, DB.events.delete);
 
-router.post("/events/request", DB.auth.verify, DB.events.request); // Open API // Public
 router.get("/events", DB.auth.verify, DB.events.get);
 router.get("/events/user", DB.auth.verify, DB.events.getByUser);
+
+// application endpoints
+router.get("/applications", DB.auth.verifyAdmin, DB.app.get); // Protected API
+router.get("/applications/:id", DB.auth.verifyAdmin, DB.app.getByID);
+
+router.get("/applications/approved", DB.auth.verifyAdmin, DB.app.getApproved);
+router.get("/applications/pending", DB.auth.verifyAdmin, DB.app.getPending);
+router.get("/applications/rejected", DB.auth.verifyAdmin, DB.app.getRejected);
+
+router.put("/applications/approve/:id", DB.auth.verifyAdmin, DB.app.approve);
+router.put("/applications/reject/:id", DB.auth.verifyAdmin, DB.app.reject);
+router.put("/applications/:id", DB.auth.verifyAdmin, DB.app.update);
+
+router.delete("/applications/:id", DB.auth.verifyAdmin, DB.app.delete);
+router.post("/applications", DB.app.create); // Open API // Public // Zero Auth
 
 // volunteer event endpoints
 router.get("/volunteer", DB.auth.verifyAdmin, DB.vol.get); // Protected API
@@ -56,7 +73,7 @@ router.get("/volunteer/:id", DB.auth.verifyAdmin, DB.vol.getByID);
 router.put("/volunteer/:id", DB.auth.verifyAdmin, DB.vol.update);
 router.delete("/volunteer/:id", DB.auth.verifyAdmin, DB.vol.delete);
 
-router.post("/volunteer", DB.vol.add); // Open API // Public
+router.post("/volunteer", DB.vol.add); // Open API // Public // Zero Auth
 
 // contact endpoints
 router.get("/contacts", DB.auth.verifyAdmin, DB.contact.get); // Protected API
@@ -65,8 +82,6 @@ router.put("/contacts/:id", DB.auth.verifyAdmin, DB.contact.update);
 router.delete("/contacts/:id", DB.auth.verifyAdmin, DB.contact.delete);
 
 router.post("/contacts", DB.auth.verify, DB.contact.create); // Open API // Server Only
-
-// router.post("/chatbot", chatbot.chatbot);
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
