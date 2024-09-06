cbddc11ff3a8fc649e9fb44d8fdc9e4fa2c80fae
seraprogrammer1 <165708692+seraprogrammer1@users.noreply.github.com>
2024-09-06 17:05:13 -0400

    update
    admin app update

--- src/components/admin/adminapp.jsx
+++ src/components/admin/adminapp.jsx
@@ -1,37 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import UserList from "./userlist";
-// import ContactList from "./contactlist";
-// import EventList from "./eventlist";
-// import VolunteerList from "./volunteerlist";
-// import ApplicationList from "./applicationlist";
-// import "./admin.css";
-
-// const AdminApp = () => {
-//   return (
-//     <div className="admin-dashboard">
-//       <h1>Admin Dashboard</h1>
-//       <div className="dashboard-section">
-//         <UserList />
-//       </div>
-//       <div className="dashboard-section">
-//         <ContactList />
-//       </div>
-//       <div className="dashboard-section">
-//         <EventList />
-//       </div>
-//       <div className="dashboard-section">
-//         <VolunteerList />
-//       </div>
-//       <div className="dashboard-section">
-//         <ApplicationList />
-//       </div>
-//     </div>
-//   );
-// };
-
-//export default AdminApp;
-
 import React, { useState } from "react";
 import UserList from "./userlist";
 import ContactList from "./contactlist";
@@ -60,8 +26,8 @@ const AdminApp = () => {
         return <VolunteerList />;
       case "ApplicationList":
         return <ApplicationList />;
-      // default:
-      //   return <UserList />;
+      default:
+        return <UserList />;
     }
   };
 
