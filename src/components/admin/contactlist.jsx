7e8e5e0918ca84785f62837a9724460d54750501
Joshua Drakeford <JD@Administrators-MacBook-Pro.local>
2024-09-05 20:07:37 -0400

    feat: ui for admin page

--- src/components/admin/contactlist.jsx
+++ src/components/admin/contactlist.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './contactlist.css';
 
 const UpdatePopup = ({ contact, onUpdate, onCancel }) => {
   const LabelInput = ({ label, name, defaultValue }) => {
--- src/components/admin/eventlist.jsx
+++ src/components/admin/eventlist.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './eventlist.css';
 
 const LabelInput = ({ label, name, defaultValue, type }) => {
   return (
